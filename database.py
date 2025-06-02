9a6cdc3ff4ee81b4a0c53b81e3e2533bb84b6022
harshmhase87 <harshmhase87@gmail.com>
2025-06-02 23:51:56 +0530

    Initial commit

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:1234@localhost/fastapi_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost/databasename"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)  # इथे SQLALCHEMY_DATABASE_URL वापरा
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
