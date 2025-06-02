0a95d5654a7b024c603e6f8ff25cbd07bc6a7976
harshmhase87 <harshmhase87@gmail.com>
2025-06-02 20:51:05 +0530

    Setup SQLAlchemy engine and session with MySQL database connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "mysql+pymysql://root:4545@localhost:3306/forone"  # Change this!
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://fastapi_user:1234@localhost/fastapi_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
